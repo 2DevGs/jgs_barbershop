a7a1b6a98bf35ff41423fa5c3f46b967b6118313
2DevGs <dev.gustavoserrano@gmail.com>
2025-10-13 13:25:26 -0300

    Integrating RestClient and Application Providers

--- lib/src/core/restClient/rest_client.dart
+++ lib/src/core/restClient/rest_client.dart
@@ -1,11 +1,29 @@
-
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:jgs_barbershop/src/config/env.dart';
 
 final class RestClient extends DioForNative {
-  RestClient() : super(BaseOptions(
-    baseUrl: Env.backendBaseUrl,
-    
-  ));
+  RestClient()
+    : super(
+        BaseOptions(
+          baseUrl: Env.backendBaseUrl,
+          connectTimeout: Duration(seconds: 10),
+          receiveTimeout: Duration(seconds: 60),
+        ),
+      ) {
+    interceptors.addAll([
+      LogInterceptor(requestBody: true, responseBody: true,),
+    ]);
+  }
+
+  RestClient get auth {
+    options.extra['DIO_AUTH_KEY'] = true;
+    return this;
+  }
+  
+  RestClient get unAuth {
+    options.extra['DIO_AUTH_KEY'] = false;
+    return this;
+  }
+
 }
