94094dd51b957e7e531abdadabd9194143da4260
2DevGs <dev.gustavoserrano@gmail.com>
2025-10-14 12:14:07 -0300

    Creating a Barbershop Repository

---
+++ lib/src/repositories/barbershop/barbershop_repository.dart
@@ -0,0 +1,8 @@
+import 'package:jgs_barbershop/src/core/exceptions/repository_exception.dart';
+import 'package:jgs_barbershop/src/core/fp/either.dart';
+import 'package:jgs_barbershop/src/model/barbershop_model.dart';
+import 'package:jgs_barbershop/src/model/user_model.dart';
+
+abstract interface class BarbershopRepository {
+  Future<Either<RepositoryException, BarbershopModel>> getMyBarbershop(UserModel userModel);
+}
