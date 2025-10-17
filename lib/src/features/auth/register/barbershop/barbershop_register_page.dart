7c6dffd4cb87a7b8ff8a28585a14d7ec77cccdc3
2DevGs <dev.gustavoserrano@gmail.com>
2025-10-17 15:44:20 -0300

    Creation of the 'Register Establishment' screen and components for the days of the week and hours panel

--- lib/src/features/auth/register/barbershop/barbershop_register_page.dart
+++ lib/src/features/auth/register/barbershop/barbershop_register_page.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/ui/widgets/hours_panel.dart';
+import '../../../../core/ui/widgets/weekdays_panel.dart';
+
 class BarbershopRegisterPage extends StatelessWidget {
+  const BarbershopRegisterPage({super.key});
 
-  const BarbershopRegisterPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: const Text('Cadastrar estabelecimento')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Nome')),
+              ),
+              const SizedBox(height: 24),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('E-mail')),
+              ),
+              const SizedBox(height: 24),
+              const SizedBox(child: WeekdaysPanel()),
+              const SizedBox(height: 24),
+              const SizedBox(child: HoursPanel(startTime: 6, endTime: 23)),
+              const SizedBox(height: 48),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(56),
+                ),
+                child: const Text('CADASTRAR ESTABELECIMENTO'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
