d828ce9c41fad0f5cd6194a9173fef428f68bc0d
2DevGs <dev.gustavoserrano@gmail.com>
2025-10-23 22:58:33 -0300

    Implementing barbershopRegisterRepository and viewModel with state

--- lib/src/core/ui/widgets/hours_panel.dart
+++ lib/src/core/ui/widgets/hours_panel.dart
@@ -5,8 +5,14 @@ import 'package:jgs_barbershop/src/core/ui/constants.dart';
 class HoursPanel extends StatelessWidget {
   final int startTime;
   final int endTime;
+  final ValueChanged<int> onHourPressed;
 
-  const HoursPanel({super.key, required this.startTime, required this.endTime});
+  const HoursPanel({
+    super.key,
+    required this.startTime,
+    required this.endTime,
+    required this.onHourPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +29,11 @@ class HoursPanel extends StatelessWidget {
           runSpacing: 16,
           children: [
             for (int i = startTime; i <= endTime; i++)
-              TimeButton(label: '${i.toString().padLeft(2, '0')}:00'),
+              TimeButton(
+                label: '${i.toString().padLeft(2, '0')}:00',
+                onPressed: onHourPressed,
+                value: i,
+              ),
           ],
         ),
       ],
@@ -31,28 +41,55 @@ class HoursPanel extends StatelessWidget {
   }
 }
 
-class TimeButton extends StatelessWidget {
+class TimeButton extends StatefulWidget {
   final String label;
+  final int value;
+  final ValueChanged<int> onPressed;
 
-  const TimeButton({super.key, required this.label});
+  const TimeButton({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.onPressed,
+  });
 
   @override
+  State<TimeButton> createState() => _TimeButtonState();
+}
+
+class _TimeButtonState extends State<TimeButton> {
+  var selected = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 64,
-      height: 38,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.white,
-        border: Border.all(color: ColorsConstants.grey),
-      ),
-      child: Center(
-        child: Text(
-          label,
-          style: const TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
-            color: ColorsConstants.grey,
+    final textColor = selected ? Colors.white : ColorsConstants.grey;
+    var buttonColor = selected ? ColorsConstants.brow : Colors.white;
+    final buttonBorderColor = selected
+        ? ColorsConstants.brow
+        : ColorsConstants.grey;
+    return InkWell(
+      borderRadius: BorderRadius.circular(8),
+      onTap: () {
+        setState(() {
+          selected = !selected;
+          widget.onPressed(widget.value);
+        });
+      },
+      child: Container(
+        width: 64,
+        height: 38,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: buttonColor,
+          border: Border.all(color: buttonBorderColor),
+        ),
+        child: Center(
+          child: Text(
+            widget.label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+              color: textColor,
+            ),
           ),
         ),
       ),
--- lib/src/features/auth/register/barbershop/barbershop_register_page.dart
+++ lib/src/features/auth/register/barbershop/barbershop_register_page.dart
@@ -1,44 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jgs_barbershop/src/core/ui/helpers/form_helper.dart';
+import 'package:jgs_barbershop/src/core/ui/helpers/messages.dart';
+import 'package:jgs_barbershop/src/features/auth/register/barbershop/barbershop_register_state.dart';
+import 'package:jgs_barbershop/src/features/auth/register/barbershop/barbershop_register_vm.dart';
+import 'package:validatorless/validatorless.dart';
 
 import '../../../../core/ui/widgets/hours_panel.dart';
 import '../../../../core/ui/widgets/weekdays_panel.dart';
 
-class BarbershopRegisterPage extends StatelessWidget {
+class BarbershopRegisterPage extends ConsumerStatefulWidget {
   const BarbershopRegisterPage({super.key});
 
   @override
+  ConsumerState<BarbershopRegisterPage> createState() =>
+      _BarbershopRegisterPageState();
+}
+
+class _BarbershopRegisterPageState
+    extends ConsumerState<BarbershopRegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  final _nameEC = TextEditingController();
+  final _emailEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _emailEC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final barbershopregisterVM = ref.watch(
+      barbershopRegisterVmProvider.notifier,
+    );
+
+    ref.listen(barbershopRegisterVmProvider, (_, state) {
+      switch (state.status) {
+        case BarbershopRegisterStateStatus.initial:
+          break;
+        case BarbershopRegisterStateStatus.error:
+          Messages.showError(
+            'Desculpe ocorreu um erro ao registrar uma barbearia',
+            context,
+          );
+          break;
+        case BarbershopRegisterStateStatus.success:
+          Navigator.of(
+            context,
+          ).pushNamedAndRemoveUntil('/home/adm', (route) => false);
+      }
+    });
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(title: const Text('Cadastrar estabelecimento')),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              TextFormField(
-                decoration: const InputDecoration(label: Text('Nome')),
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                decoration: const InputDecoration(label: Text('E-mail')),
-              ),
-              const SizedBox(height: 24),
-              WeekdaysPanel(onDayPressed: (value) {
-                print('Dia selecionado: $value');
-              },),
-              const SizedBox(height: 24),
-              HoursPanel(startTime: 6, endTime: 23),
-              const SizedBox(height: 48),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(56),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _nameEC,
+                  validator: Validatorless.required('Nome obrigatório'),
+                  onTapOutside: (_) => context.unFocus(),
+                  decoration: const InputDecoration(label: Text('Nome')),
                 ),
-                child: const Text('CADASTRAR ESTABELECIMENTO'),
-              ),
-            ],
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: _emailEC,
+                  validator: Validatorless.multiple([
+                    Validatorless.required('E-mail obrigatório'),
+                    Validatorless.email('E-mail inválido'),
+                  ]),
+                  onTapOutside: (_) => context.unFocus(),
+                  decoration: const InputDecoration(label: Text('E-mail')),
+                ),
+                const SizedBox(height: 24),
+                WeekdaysPanel(
+                  onDayPressed: (value) {
+                    barbershopregisterVM.addOrRemoveOpenDay(value);
+                  },
+                ),
+                const SizedBox(height: 24),
+                HoursPanel(
+                  startTime: 6,
+                  endTime: 23,
+                  onHourPressed: (value) {
+                    barbershopregisterVM.addOrRemoveOpenHour(value);
+                  },
+                ),
+                const SizedBox(height: 48),
+                ElevatedButton(
+                  onPressed: () {
+                    switch (formKey.currentState?.validate()) {
+                      case false || null:
+                        Messages.showError('Formulário inválido', context);
+                      case true:
+                        barbershopregisterVM.register(
+                          _nameEC.text,
+                          _emailEC.text,
+                        );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(56),
+                  ),
+                  child: const Text('CADASTRAR ESTABELECIMENTO'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
