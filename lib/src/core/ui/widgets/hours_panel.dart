79ab15274967ebecef69e3aa29eb29c4815ba02b
2DevGs <dev.gustavoserrano@gmail.com>
2025-11-03 17:23:37 -0300

    Customizing the "hoursPanel" to allow single time selection
    To work on the "Schedule client" screen.

--- lib/src/core/ui/widgets/hours_panel.dart
+++ lib/src/core/ui/widgets/hours_panel.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:jgs_barbershop/src/core/ui/constants.dart';
 
-class HoursPanel extends StatelessWidget {
+class HoursPanel extends StatefulWidget {
   final List<int>? enabledTimes;
   final int startTime;
   final int endTime;
   final ValueChanged<int> onHourPressed;
+  final bool singleSelection;
 
   const HoursPanel({
     super.key,
@@ -14,10 +15,26 @@ class HoursPanel extends StatelessWidget {
     required this.startTime,
     required this.endTime,
     required this.onHourPressed,
-  });
+  }) : singleSelection = false;
+
+  const HoursPanel.singleSelection({
+    super.key,
+    this.enabledTimes,
+    required this.startTime,
+    required this.endTime,
+    required this.onHourPressed,
+  }) : singleSelection = true;
+
+  @override
+  State<HoursPanel> createState() => _HoursPanelState();
+}
+
+class _HoursPanelState extends State<HoursPanel> {
+  int? lastSelection;
 
   @override
   Widget build(BuildContext context) {
+    final HoursPanel(:singleSelection) = widget;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -30,12 +47,25 @@ class HoursPanel extends StatelessWidget {
           spacing: 8,
           runSpacing: 16,
           children: [
-            for (int i = startTime; i <= endTime; i++)
+            for (int i = widget.startTime; i <= widget.endTime; i++)
               TimeButton(
-                enabledTimes: enabledTimes,
+                enabledTimes: widget.enabledTimes,
                 label: '${i.toString().padLeft(2, '0')}:00',
-                onPressed: onHourPressed,
                 value: i,
+                timeSelected: lastSelection,
+                singleSelection: singleSelection,
+                onPressed: (timeSelected) {
+                  setState(() {
+                    if (singleSelection) {
+                      if (lastSelection == timeSelected) {
+                        lastSelection = null;
+                      } else {
+                        lastSelection = timeSelected;
+                      }
+                    }
+                  });
+                  widget.onHourPressed(timeSelected);
+                },
               ),
           ],
         ),
@@ -49,6 +79,8 @@ class TimeButton extends StatefulWidget {
   final String label;
   final int value;
   final ValueChanged<int> onPressed;
+  final bool singleSelection;
+  final int? timeSelected;
 
   const TimeButton({
     super.key,
@@ -56,6 +88,8 @@ class TimeButton extends StatefulWidget {
     required this.label,
     required this.value,
     required this.onPressed,
+    required this.singleSelection,
+    required this.timeSelected,
   });
 
   @override
@@ -66,28 +100,46 @@ class _TimeButtonState extends State<TimeButton> {
   var selected = false;
   @override
   Widget build(BuildContext context) {
+    final TimeButton(
+      :value,
+      :label,
+      :enabledTimes,
+      :onPressed,
+      :singleSelection,
+      :timeSelected,
+    ) = widget;
+    if (singleSelection) {
+      if (timeSelected != null) {
+        if (timeSelected == value) {
+          selected = true;
+        } else {
+          selected = false;
+        }
+      }
+    }
+
     final textColor = selected ? Colors.white : ColorsConstants.grey;
     var buttonColor = selected ? ColorsConstants.brow : Colors.white;
     final buttonBorderColor = selected
         ? ColorsConstants.brow
         : ColorsConstants.grey;
 
-    final TimeButton(:value, :label, :enabledTimes, :onPressed) = widget;
-    
     final disabledTime = enabledTimes != null && !enabledTimes.contains(value);
 
-    if(disabledTime) {
+    if (disabledTime) {
       buttonColor = Colors.grey[400]!;
     }
 
     return InkWell(
       borderRadius: BorderRadius.circular(8),
-      onTap: disabledTime ? null : () {
-        setState(() {
-          selected = !selected;
-          onPressed(value);
-        });
-      },
+      onTap: disabledTime
+          ? null
+          : () {
+              setState(() {
+                selected = !selected;
+                onPressed(value);
+              });
+            },
       child: Container(
         width: 64,
         height: 38,
