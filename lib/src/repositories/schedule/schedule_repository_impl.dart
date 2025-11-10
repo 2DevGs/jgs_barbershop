9a845e72a6d4921e05f02c3564968efaddbb7df6
2DevGs <dev.gustavoserrano@gmail.com>
2025-11-10 16:22:04 -0300

    Implementing scheduleRepository.findRepositoryByName

--- lib/src/repositories/schedule/schedule_repository_impl.dart
+++ lib/src/repositories/schedule/schedule_repository_impl.dart
@@ -5,6 +5,7 @@ import 'package:jgs_barbershop/src/core/exceptions/repository_exception.dart';
 import 'package:jgs_barbershop/src/core/fp/either.dart';
 import 'package:jgs_barbershop/src/core/fp/nil.dart';
 import 'package:jgs_barbershop/src/core/restClient/rest_client.dart';
+import 'package:jgs_barbershop/src/model/schedule_model.dart';
 
 import './schedule_repository.dart';
 
@@ -33,6 +34,31 @@ class ScheduleRepositoryImpl implements ScheduleRepository {
     } on DioException catch (e, s) {
       log('Erro ao registrar agendamento', error: e, stackTrace: s);
       return Failure(RepositoryException(message: 'Erro ao agendar horário'));
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, List<ScheduleModel>>> findScheduleByDate(
+    ({DateTime date, int userId}) filter,
+  ) async {
+    try {
+      final Response(:List data) = await restClient.auth.get(
+        '/schedules',
+        queryParameters: {
+          'user_id': filter.userId,
+          'date': filter.date.toIso8601String(),
+        },
+      );
+      final schedules = data.map((s) => ScheduleModel.fromMap(s)).toList();
+      return Success(schedules);
+    } on DioException catch (e, s) {
+      log('Erro ao buscar agendamentos de uma data', error: e, stackTrace: s);
+      return Failure(
+        RepositoryException(message: 'Erro ao buscar agendamentos de uma data'),
+      );
+    } on ArgumentError catch (e, s) {
+      log('Json inválido', error: e, stackTrace: s);
+      return Failure(RepositoryException(message: 'Json inválido'));
     }
   }
 }
